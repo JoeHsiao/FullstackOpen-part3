0dcbe4b255156e95725ce201679acd9713f07875
Joe Hsiao <joe.phsiao@gmail.com>
2025-02-10 12:01:27 -0800

    complete 3.10

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post('/api/persons', (request, response) => {
 app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people <br> ${new Date().toUTCString()}`)
 })
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log('Server running')
 })
